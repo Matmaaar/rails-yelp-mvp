0a45a6256016195791cd234081a1d3a28ab8e6ef
Matis Martin <matis.martin@outlook.fr>
2025-05-21 15:27:18 +0200

    terminé

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
+  RATINGS = %w(0 1 2 3 4 5)
+
   belongs_to :restaurant
   validates :content, presence: true
   validates :restaurant_id, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,43 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+  resto_1 = Restaurant.new(
+    name:    "resto 1",
+    address: "Nantes",
+    category: "chinese",
+    
+  )
+  resto_1.save!
+
+  resto_2 = Restaurant.new(
+    name:    "resto 2",
+    address: "Bordeaux",
+    category: "italian",
+
+  )
+  resto_2.save!
+
+  resto_3 = Restaurant.new(
+    name:    "resto 3",
+    address: "Bruxelles",
+    category: "japanese",
+
+  )
+  resto_3.save!
+
+  resto_4 = Restaurant.new(
+    name:    "resto 4",
+    address: "Nantes",
+    category: "french",
+
+  )
+  resto_4.save!
+
+  resto_5 = Restaurant.new(
+    name:    "resto 5",
+    address: "Super Ville",
+    category: "belgian",
+
+  )
+  resto_5.save!
